fe7fbe7812d765037ff28f411b94d0fa077d7122
Maxine Janka <maxine.janka@gmail.com>
2024-02-28 12:55:19 +1100

    Push to GitHub

---
+++ js/main.js
@@ -0,0 +1,16 @@
+console.log("hello");
+
+function codingBoss() {
+	alert("Hello World");
+} 
+//The alert hasnt popped up because the function has not been called
+
+codingBoss(); //Function has been called, now the alert pops up
+
+document.querySelector('body').style.backgroundColor = "hotpink"; 
+//Here we are calling the body and targeting the background color
+
+document.querySelector('h1').style.color = "yellow";
+
+document.querySelector('.button').style.backgroundColor = "orange";
+//can target a class. Remember to use camelCase syntax when targetting stuff - backgroundColor
